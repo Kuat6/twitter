50a7450389d253ce9b9df535e97da87673d183cf
Kuat Konirbayev <magistr135@gmail.com>
2014-08-21 10:56:04 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,21 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
-end  
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      # Handle a successful save.
-    else
-      render 'new'
-    end
-  end
-
-  private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
   
 end
