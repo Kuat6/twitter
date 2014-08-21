04e418095858d88e64e087054c73c3aaf178ad18
Kuat Konirbayev <magistr135@gmail.com>
2014-08-21 12:00:44 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+    has_many :microposts, dependent: :destroy
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
@@ -11,6 +13,11 @@ class User < ActiveRecord::Base
   
   def User.new_remember_token
     SecureRandom.urlsafe_base64
+  end
+  
+    def feed
+    # Это предварительное решение. См. полную реализацию в "Following users".
+    Micropost.where("user_id = ?", id)
   end
 
   def User.encrypt(token)
